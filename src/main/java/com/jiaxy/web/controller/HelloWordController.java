3b1e9f8b6e959815d2bab25d1e6b0d16718f96ae
thomas <taobaorun@gmail.com>
2017-01-14 23:45:07 +0800

    init

---
+++ src/main/java/com/jiaxy/web/controller/HelloWordController.java
@@ -0,0 +1,17 @@
+package com.jiaxy.web.controller;
+
+/**
+ * Title: <br>
+ * <p>
+ * Description: <br>
+ * <p>
+ * </p>
+ * <br>
+ *
+ * @author <a href=mailto:taobaorun@gmail.com>taobaorun</a>
+ *         <br>
+ * @since 2017/1/14 23:44
+ */
+public class HelloWordController {
+
+}
